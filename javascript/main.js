2688638f105104147df40df0d541ed5b26b6d621
reduno96 <reduno96@gmail.com>
2023-01-31 13:30:42 +0100

    add javascript File

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,31 @@
+let el = document.querySelector(".scroller");
+let height =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+window.addEventListener("scroll", () => {
+  let scrollTop = document.documentElement.scrollTop;
+  el.style.width = `${(scrollTop / height) * 100}%`;
+});
+
 const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener("click", () => {
-    primaryNav.hasAttribute("data-visible")
+  primaryNav.hasAttribute("data-visible")
     ? navToggle.setAttribute("aria-expanded", false)
     : navToggle.setAttribute("aria-expanded", true);
-    primaryNav.toggleAttribute("data-visible");
-    primaryHeader.toggleAttribute("data-overlay");
-})
-  const slider = new A11YSlider(document.querySelector('.slider'), {
-    adaptiveHeight: false,
-    dots: true,
-    centerMode: true,
-    arrows: false, 
-    responsive: {
-        480: {
-            dots: false,
-        },
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+});
+const slider = new A11YSlider(document.querySelector(".slider"), {
+  adaptiveHeight: false,
+  dots: true,
+  centerMode: true,
+  arrows: false,
+  responsive: {
+    480: {
+      dots: false,
     },
+  },
 });
